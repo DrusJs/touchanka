07878f00197ec44aa7b9505a85fc98ef33b3c21b
DrusJs <magnojoky962@gmail.com>
2024-05-14 15:00:52 +0300

    add animation scroll

--- js/main.js
+++ js/main.js
@@ -15,6 +15,34 @@ function accordionToggleEvent(e) {
     }
 }
 
+function checkBlocksVisibility() {
+    let windowHeight = window.innerHeight;
+    let blocks = document.querySelectorAll('.fade');
+    let delayItem = window.matchMedia("(max-width: 800px)").matches ? 40 : 80
+    let accordion = document.querySelector('.faq-item.active')
+    let selects = document.querySelectorAll('.select-primary.active')
+
+    blocks.forEach(block => {
+        let blockPosition = block.getBoundingClientRect().top;
+
+        if (blockPosition < windowHeight - delayItem) {
+            block.style.opacity = "1";
+            block.style.transform = "translateY(0)";
+        }
+    });
+    if (accordion) {
+        this.isVisible(accordion)
+    }
+    if (selects.length !== 0) {
+        selects.forEach(el => {
+            this.isVisible(el)
+        })
+    }
+}
+checkBlocksVisibility()
+
+window.addEventListener('scroll', checkBlocksVisibility)
+
 if (document.querySelector('.faq-accordion')) {
     document.querySelectorAll('.faq-accordion-head').forEach((accordion) => accordion.addEventListener('click', accordionToggleEvent))
 }
